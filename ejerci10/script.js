274530276c4932500903aac8b1a12100dfff5c65
marisol perez <107561805+marisolnoeliaperez@users.noreply.github.com>
2022-09-18 21:19:46 -0300

    Add files via upload

---
+++ ejerci10/script.js
@@ -0,0 +1,74 @@
+let nombre =  ["programacion" , " facil "]; 
+ let saludo = "les doy la bienvenida a ";
+ let comision = "de comision e";
+
+ let fraseCompleta = saludo + nombre + comision ; 
+ console.log (fraseCompleta) ;
+
+
+
+ let frase = "JavaScript " + "es un lenguaje" + "esencial para crear" + " sitios web poderosos.";
+ console.log(frase);
+
+
+ let numeroString1 = " numero " + "10";
+ let numeroString2 = " numero " + "20";
+ let numeroString3 = " numero " + "30";
+
+ let concatenaNumeros = numeroString1 + numeroString2 + numeroString3;
+
+ console.log(concatenaNumeros);
+
+
+ let nombre2 = prompt("¿cual es tu nombre y apellido?");
+ let edadd =prompt("¿cual es tu edad?");
+
+ console.warn(nombre2  + edadd);
+
+
+
+
+
+
+ let edad = prompt("Introduce tu edad");
+ let sexo = prompt("introduce tu sexo");
+
+
+if ((sexo == "femenino") && (edad >= 20) ){
+alert("Puedes entrar, eres mayor de edad.");
+}
+else if ( (sexo == "masculino") && (edad >= 18)){
+    alert("Puedes entrar, eres mayor de edad.");
+    }       
+else 
+{
+alert("No puedes entrar, eres menor de edad.");
+}
+    
+
+
+
+
+
+
+let edaad = prompt("Introduce tu edad");
+
+
+
+if  (edad >= 20) {
+alert("Puedes entrar, eres mayor de edad.");
+}
+else if  (edad >= 18){
+   alert("Puedes entrar, eres mayor de edad.");
+   }       
+else 
+{
+alert("No puedes entrar, eres menor de edad.");
+}
+   
+
+
+
+
+
+
